3760a24e18e9fb61601da374eace76a9f0a848ac
Bloo <khanh.mdang72@gmail.com>
2021-02-01 00:59:37 +0700

    updating shop page and adding data

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MenuItem from '../../components/menu-item/menu-item.component';
+import SECTIONS_DATA from '../../sections.data';
 import './directory.styles.scss';
 
 class Directory extends Component {
@@ -7,40 +8,7 @@ class Directory extends Component {
     super();
 
     this.state = {
-      sections: [
-        {
-          title: 'HATS',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'hats'
-        },
-        {
-          title: 'JACKETS',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: 'shop/jackets'
-        },
-        {
-          title: 'SNEAKERS',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: 'shop/sneakers'
-        },
-        {
-          title: 'WOMEN',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens'
-        },
-        {
-          title: 'MEN',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens'
-        }
-      ]
+      sections: SECTIONS_DATA
     };
   };
 
